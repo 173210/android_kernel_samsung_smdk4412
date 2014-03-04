0832e3a7877dd2b6711e24fbea94feb115a81aa2
173210 <root.3.173210@live.com>
2014-03-04 23:07:04 +0900

    Updated build tools and settings

--- build.sh
+++ build.sh
@@ -1,21 +1,52 @@
-rm /home/gustavo/zips/*.zip
-cp /home/gustavo/kernel/arch/arm/configs/kernel_defconfig /home/gustavo/kernel/.config
+#!/bin/bash
+
+KERNEL_DIR=$PWD
+#TOOLCHAIN_DIR=$HOME/toolchain
+
+if [ -n "$1" ]; then
+	OBJ_DIR=$1
+else
+	OBJ_DIR=$HOME
+fi
+if [ ! -e $OBJ_DIR ]; then
+	mkdir $OBJ_DIR
+fi
+if [ ! -e $OBJ_DIR/zips ]; then
+	mkdir $OBJ_DIR/zips
+fi
+if [ -e $OBJ_DIR/zip_tmp ]; then
+	rm $OBJ_DIR/zip_tmp/*
+else
+	mkdir $OBJ_DIR/zip_tmp
+fi
+
+if [ -n "$2" ]; then
+	DEFCONFIG=$2
+else
+	DEFCONFIG=kernel_defconfig
+fi
+
+if [ -e $TOOLCHAIN_DIR ]; then
+	PATH=$PATH:$TOOLCHAIN_DIR/bin
+fi
+if [ ! -n $CROSS_COMPILE ]; then
+	CROSS_COMPILE=arm-gnueabi-
+fi
+
+cp $KERNEL_DIR/arch/arm/configs/$DEFCONFIG $KERNEL_DIR/.config
 make -j3
-find -name '*.ko' -exec cp -av {} /home/gustavo/kernel/usr/galaxys2_initramfs_files/modules/ \;
-chmod 644 /home/gustavo/kernel/usr/galaxys2_initramfs_files/modules/*
-/home/gustavo/toolchain/bin/arm-gnueabi-strip --strip-unneeded /home/gustavo/kernel/usr/galaxys2_initramfs_files/modules/*
-make -j3 zImage CONFIG_INITRAMFS_SOURCE="/home/gustavo/kernel/usr/initramfs/cwm.list"
-cp /home/gustavo/kernel/arch/arm/boot/zImage /home/gustavo/zip_tmp/
+find -name '*.ko' -exec cp -av {} $KERNEL_DIR/usr/galaxys2_initramfs_files/modules \;
+chmod 644 $KERNEL_DIR/usr/galaxys2_initramfs_files/modules/*
+${CROSS_COMPILE}strip --strip-unneeded $KERNEL_DIR/usr/galaxys2_initramfs_files/modules/*
+
+make -j3 zImage CONFIG_INITRAMFS_SOURCE="$KERNEL_DIR/usr/initramfs/cwm.list"
+cp $KERNEL_DIR/arch/arm/boot/zImage $OBJ_DIR/zip_tmp
 CURRENTDATE=$(date +"%d-%m")
-cd /home/gustavo/zip_tmp
-rm *.zip
-zip -r kk-kernel-$CURRENTDATE-CWM.zip ./
-cp ./*.zip /home/gustavo/zips/kk-kernel-$CURRENTDATE-CWM.zip
-cd /home/gustavo/kernel
-make -j3 zImage CONFIG_INITRAMFS_SOURCE="/home/gustavo/kernel/usr/initramfs/twrp.list"
-cp /home/gustavo/kernel/arch/arm/boot/zImage /home/gustavo/zip_tmp/
+zip -jr $OBJ_DIR/zips/kk-kernel-$CURRENTDATE-CWM.zip $OBJ_DIR/zip_tmp
+
+rm $OBJ_DIR/zip_tmp/*
+
+make -j3 zImage CONFIG_INITRAMFS_SOURCE="$KERNEL_DIR/usr/initramfs/twrp.list"
+cp $KERNEL_DIR/arch/arm/boot/zImage $OBJ_DIR/zip_tmp
 CURRENTDATE=$(date +"%d-%m")
-cd /home/gustavo/zip_tmp
-rm *.zip
-zip -r kk-kernel-$CURRENTDATE-TWRP.zip ./
-cp ./*.zip /home/gustavo/zips/kk-kernel-$CURRENTDATE-TWRP.zip
+zip -jr $OBJ_DIR/zips/kk-kernel-$CURRENTDATE-TWRP.zip $OBJ_DIR/zip_tmp
